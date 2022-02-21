972c584a441c8a5f66c25c5f7180a61d5d75d40b
SabriMahmoud <sabri.mahmoud@eniso.u-sousse.tn>
2022-02-21 11:54:13 +0100

    project creation

---
+++ Consent_Management_Platform/src/main/java/com/example/gdpr/ConsentManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsentManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsentManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ Consent_Management_Platform/src/test/java/com/example/gdpr/ConsentManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsentManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
