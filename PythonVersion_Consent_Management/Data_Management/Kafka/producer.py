6233f0032356a6bb14dcb3f4a904e6ca0999456b
SabriMahmoud <sabri.mahmoud@eniso.u-sousse.tn>
2022-06-20 21:58:57 +0200

    script file changed test

--- PythonVersion_Consent_Management/Data_Management/Kafka/producer.py
+++ PythonVersion_Consent_Management/Data_Management/Kafka/producer.py
@@ -2,14 +2,26 @@ import json
 import time
 
 from kafka import KafkaProducer
+import argparse
 
-KAFKA_TOPIC = "evt"
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t","--topic",type=str,default="",help="Type topic name" )
+arguments = parser.parse_args()
+KAFKA_TOPIC = arguments.topic
+
+print(KAFKA_TOPIC)
+KAFKA_TOPIC = "events"
+     
 
 
 producer = KafkaProducer(bootstrap_servers="localhost:9092")
+print("===================================================\n")
 
-print("Going to be generating data after 10 seconds")
-print("Will generate one unique order every 2 seconds")
+print("Going to be generating data after 5 seconds")
+print("Will generate one unique order every 2 seconds\n")
+
 time.sleep(5)
 
 # Retriving data from json file 
@@ -17,12 +29,13 @@ time.sleep(5)
 # Modify this function as needed el moufid raja3 list 
 
 def read_from_data_source(file_name):
-    with open('kafka/'+file_name) as data_source :
+    with open(file_name) as data_source :
         data = json.load(data_source)
     return data 
 
 data = read_from_data_source('MOCK_DATA.json')
 
+print("===================================================\n")
 
 for i in range(len(data)):
     producer.send(KAFKA_TOPIC, json.dumps(data[i]).encode("utf-8"))
