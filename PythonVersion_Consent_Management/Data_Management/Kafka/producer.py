364471a24bc569c421232ea896be435a1b073e97
SabriMahmoud <sabri.mahmoud@eniso.u-sousse.tn>
2022-06-21 10:49:51 +0200

    ignore dir deleted

--- PythonVersion_Consent_Management/Data_Management/Kafka/producer.py
+++ PythonVersion_Consent_Management/Data_Management/Kafka/producer.py
@@ -2,19 +2,23 @@ import json
 import time
 from kafka import KafkaProducer
 import argparse
+from encrypt_with_vault import EncryptionDecryptionUnit
 
 
+EDU = EncryptionDecryptionUnit('http://localhost:8200', token = 'myroot',transit="Test")
+EDU.create_key("bankerise_key")
+EDU.encryptData("bankerise_key","hello")
+ciphertext= EDU.ciphertext
+print(ciphertext)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t","--topic",type=str,default="",help="Type topic name" )
 arguments = parser.parse_args()
 KAFKA_TOPIC = arguments.topic
 
-print(KAFKA_TOPIC)
-KAFKA_TOPIC = "events"
-     
-
-
+if KAFKA_TOPIC=="":
+    KAFKA_TOPIC = "events"
+    
 producer = KafkaProducer(bootstrap_servers="localhost:9092")
 print("===================================================\n")
 
@@ -31,13 +35,25 @@ def read_from_data_source(file_name):
     with open(file_name) as data_source :
         data = json.load(data_source)
     return data 
+def encrypt_dict(row):
+    for key in row.keys():
+        plaintext = row[key]
+        EDU.encryptData("bankerise_key",str(plaintext))
+        row[key] = EDU.ciphertext
+
+    return row 
+
 
 data = read_from_data_source('MOCK_DATA.json')
+
+
+
+
 
 print("===================================================\n")
 
 for i in range(len(data)):
-    producer.send(KAFKA_TOPIC, json.dumps(data[i]).encode("utf-8"))
+    producer.send(KAFKA_TOPIC, json.dumps(encrypt_dict(data[i])).encode("utf-8"))
     print(f"Done Sending..{i}")
     print(f"Remaining .. {len(data)-i-1}")
     time.sleep(1)
